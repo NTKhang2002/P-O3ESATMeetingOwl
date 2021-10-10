aec50adbb62ee155b1c263b08d8d0e1ed7ee1844
siebeschroeders <siebe.schroeders@gmail.com>
2021-10-10 16:57:13 +0200

    OpenCV met mp4 als test

--- main2.py
+++ main2.py
@@ -2,11 +2,11 @@ import cv2
 
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
+mouth_cascade = cv2.CascadeClassifier('haarcascade_mcs_mouth.xml')
 # To capture video from webcam.
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 # To use a video file as input
-# cap = cv2.VideoCapture('filename.mp4')
+cap = cv2.VideoCapture('input.mp4')
 
 while True:
     # Read the frame
@@ -15,8 +15,11 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detect the faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    mouths = mouth_cascade.detectMultiScale(gray, 1.1, 4)
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    for (x, y, w, h) in mouths:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
     # Display
     cv2.imshow('img', img)
