a995e9b76163de6d96a28e7fed7e54490e1d6d5c
JorenVanGorp <joren.vangorp@gmail.com>
2021-10-31 21:58:45 +0100

    update

--- Joren1/coordinaatzoom.py
+++ Joren1/coordinaatzoom.py
@@ -29,9 +29,9 @@ def zoom(img,faces, face=0):
         xfc = X + W/2
         yfc = Y + H/2
 
-        ymin = max(min(int(yfc-H)-1,int(Ry-2*H)),0)
+        ymin = max(min(int(yfc-H),int(Ry-2*H)),0)
         ymax = max(min(int(yfc+H),Ry),int(2*H))
-        xmin = max(min(int(xfc-V*H)-1,int(Rx-V*2*H)),0)
+        xmin = max(min(int(xfc-V*H),int(Rx-V*2*H)),0)
         xmax = max(min(int(xfc+V*H),Rx),int(V*2*H))
 
 
