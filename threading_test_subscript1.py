751bed3336f3790d022f1b015484474d1dc46609
NTKhang2002 <trungkhang.nguyen@gmail.com>
2021-11-09 17:21:51 +0100

    mediapipe

--- threading_test_subscript1.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Script 1 is running...")
-
-if __name__ == '__main__':
-    main()
--- threading_test_subscript2.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Script 2 is running...")
-
-if __name__ == '__main__':
-    main()
