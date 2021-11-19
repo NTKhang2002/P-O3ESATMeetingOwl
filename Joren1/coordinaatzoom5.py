8c8e59b91dc927149a3f4346bc08a8be8cef3ce0
JorenVanGorp <joren.vangorp@gmail.com>
2021-11-19 17:17:09 +0100

    zoom

--- Joren1/coordinaatzoom5.py
+++ Joren1/coordinaatzoom5.py
@@ -1,13 +1,14 @@
 import cv2
 
-HEIGHT = 480
+HEIGHT = 720
 WIDTH = int(16 / 9 * HEIGHT)
 MIDDLEPOINTX = int(WIDTH/2)
 MIDDLEPOINTY = int(HEIGHT/2)
 tijd = 15
+schaal = 1.3
 interpolatielijst = [0]*tijd
 # 1/2 for full screen[|---------------------|] , 1/4 to use only the middle part [-----|----------|-----]
-Central_bounding = int(1/4 * WIDTH)
+Central_bounding = int(1/2 * WIDTH)
 
 # creating a variable with the classifiers
 CLASSIFIERS = "haarcascade_frontalface_default.xml"
@@ -15,7 +16,7 @@ CLASSIFIERS = "haarcascade_frontalface_default.xml"
 # Create cascade
 FaceCascade = cv2.CascadeClassifier(CLASSIFIERS)
 # Capture from camera, 0 because webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
@@ -25,7 +26,7 @@ Xf, Yf, Wf, Hf = MIDDLEPOINTX, MIDDLEPOINTY, MIDDLEPOINTX, MIDDLEPOINTY
 xmin, xmax, ymin, ymax = 0, WIDTH, 0, HEIGHT
 
 
-def zoomboundaries(img , X, Y, W, H):
+def zoomboundaries(img , X, Y, W, H,Scalar):
     Ry = img.shape[0]
     Rx = img.shape[1]
     V = Rx / Ry
@@ -33,10 +34,10 @@ def zoomboundaries(img , X, Y, W, H):
     xfc = X + W / 2
     yfc = Y + H / 2
 
-    ymin = max(min(int(yfc - H), int(Ry - 2 * H)), 0)
-    ymax = max(min(int(yfc + H), Ry), int(2 * H))
-    xmin = max(min(int(xfc - V * H), int(Rx - V * 2 * H)), 0)
-    xmax = max(min(int(xfc + V * H), Rx), int(V * 2 * H))
+    ymin = max(min(int(yfc - Scalar*H), int(Ry - 2 * Scalar*H)), 0)
+    ymax = max(min(int(yfc + Scalar*H), Ry), int(2 * Scalar*H))
+    xmin = max(min(int(xfc - V * Scalar * H), int(Rx - V * 2 * Scalar * H)), 0)
+    xmax = max(min(int(xfc + V * Scalar * H), Rx), int(V * 2 * Scalar * H))
     return xmin, xmax, ymin, ymax
 
 def crop(img, xmin, xmax, ymin, ymax):
@@ -100,7 +101,7 @@ while True:
             Hf = gemiddeldelijst(interpolatielijst,3)
 
         if faces != () and len(faces) >= face + 1:
-            (xmin, xmax, ymin, ymax) = zoomboundaries(img, Xf, Yf, Wf, Hf)
+            (xmin, xmax, ymin, ymax) = zoomboundaries(img, Xf, Yf, Wf, Hf,schaal)
 
     imgcropped = crop(img, xmin, xmax, ymin, ymax)
     imgresized = resizer(imgcropped, WIDTH, HEIGHT)
