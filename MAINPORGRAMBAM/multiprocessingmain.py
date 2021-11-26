8a4aabb92bcbb56e0c081d8e75af577ff61652ba
JorenVanGorp <joren.vangorp@gmail.com>
2021-11-26 16:49:42 +0100

    mainprogram

--- MAINPORGRAMBAM/multiprocessingmain.py
+++ MAINPORGRAMBAM/multiprocessingmain.py
@@ -1,12 +1,13 @@
 import multiprocessing
-from facedetection import facerecognition
-from coordinaatzoom6 import coordinaatzoomfunctie
+from main_program_test_final import pipeline
+from coordinaatzoom import coordinaatzoomfunctie
 
 
 
 def main():
     p2 = multiprocessing.Process(target=coordinaatzoomfunctie)
-    p1 = multiprocessing.Process(target=facerecognition)
+    p1 = multiprocessing.Process(target=pipeline)
+
 
     p2.start()
     p1.start()
