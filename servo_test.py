c3603ccfaeaac8555e714b45c2320c977ec470fa
Maanoe <Maanoe.neven@gmail.com>
2021-11-30 15:38:10 +0100

    servo test tegoei

--- servo_test.py
+++ servo_test.py
@@ -6,8 +6,12 @@ import math
 
 
 def move(x,x_oud,nodeMcu,straal,helft_pixels):
+    print("x: " + str(x))
+    print("x_oud: " + str(x_oud))
     if x != None:
-        if abs(x_oud - x) > 75:
+
+        if abs(x_oud - x) > 150:
+            x_oud = x
             if x > helft_pixels:
                 x = x-helft_pixels
                 angle = 90 - (math.sin(x / straal))*180/3.14
@@ -18,7 +22,7 @@ def move(x,x_oud,nodeMcu,straal,helft_pixels):
                 angle = 90
             print(angle)
             print(math.sin(x / straal)*180/3.14)
-            x = x_oud
+
 
             nodeMcu.write(str(angle).encode())
     return x_oud
