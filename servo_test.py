cd0ee18417727ab69df732971e434eb30eca7974
NTKhang2002 <trungkhang.nguyen@gmail.com>
2021-11-30 15:31:49 +0100

    arduino

--- servo_test.py
+++ servo_test.py
@@ -7,7 +7,7 @@ import math
 
 def move(x,x_oud,nodeMcu,straal,helft_pixels):
     if x != None:
-        if abs(x_oud - x) > 75:
+        if abs(x_oud - x) > 150:
             if x > helft_pixels:
                 x = x-helft_pixels
                 angle = 90 - (math.sin(x / straal))*180/3.14
@@ -18,7 +18,7 @@ def move(x,x_oud,nodeMcu,straal,helft_pixels):
                 angle = 90
             print(angle)
             print(math.sin(x / straal)*180/3.14)
-            x = x_oud
+            x_oud = x
 
             nodeMcu.write(str(angle).encode())
     return x_oud
